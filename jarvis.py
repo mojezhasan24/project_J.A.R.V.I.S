a6068d5e10bd8aa49bdb627297b5d285a153dcf6
mojezhasan24 <mojezhasan@outlook.com>
2025-04-19 00:37:23 +0530

    ititial-commit

--- jarvis.py
+++ jarvis.py
@@ -1,3 +1,9 @@
+import pyttsx3
+engine = pyttsx3.init('espeak', debug=True)
+voices = engine.getProperty('voices')
+print(voices[1].id)
 def speak(audio):
-    pass
-    
+    engine.say(audio)
+    engine.runAndWait()
+if __name__ == "__main__":
+    speak("Hello, I am Jarvis, your personal assistant.")
