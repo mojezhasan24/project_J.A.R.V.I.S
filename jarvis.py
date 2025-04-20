3b6910c260de4b7c99f0e27406d9f2e82cf5d16a
mojezhasan24 <mojezhasan@outlook.com>
2025-04-20 12:38:22 +0530

    added more robust controles

--- jarvis.py
+++ jarvis.py
@@ -2,7 +2,8 @@ import datetime
 import subprocess
 import speech_recognition as sr
 import time
-
+import wikipedia
+import webbrowser
 
 def speak(audio):
     subprocess.call(["spd-say", audio])
@@ -25,7 +26,7 @@ def wishMe():
         print("Executing evening greeting...")  # Debugging
         speak("Good evening sir!")
         time.sleep(1)
-        speak("I am Jarvis, your personal A.I. assistant. How can I help you today()")
+        speak("I am Jarvis, your personal A. I. assistant. How can I help you today?")
 
 def takeCommand():
     # This function will take microphone input and return the string output
@@ -40,7 +41,7 @@ def takeCommand():
         r.phrase_threshold = 1 # minimum seconds of speaking audio before we consider the speaking audio a phrase - values below this are ignored (for filtering out clicks and pops)
         
         audio = r.listen(source)
-
+    time.sleep(1)
     try:
         print("Recognizing...")
         query = r.recognize_google(audio, language='en-in')
@@ -53,6 +54,28 @@ def takeCommand():
 
 if __name__ == "__main__":
     wishMe()
-    query = takeCommand().lower()
-    
-    
+    time.sleep(2)
+    while True:
+        query = takeCommand().lower()
+        if 'wikipedia' in query or 'wiki' in query:
+            # speak("Searching Wikipedia for...")
+            sub_query = query.replace("wikipedia", "").replace("wiki", "").strip()
+            results = wikipedia.summary(sub_query, sentences=2)
+
+            speak(f"serching wikipedia for {sub_query}")
+            # results = wikipedia.summary(query, sentences=2)
+            # speak("according to wikipedia")
+            print(results)
+            speak(results)
+            time.sleep(20)
+            speak("sir, do you want me to search it on youtube too")
+            query2=takeCommand().lower()
+            if "yes" in query2:
+                speak("opening youtube")
+                webbrowser.open("youtube.com")
+        elif 'open youtube' in query:
+            speak("opening youtube")
+            webbrowser.open("youtube.com")
+        elif "exit" in query or "quit" in query:
+            speak("Goodbye sir!")
+            break
